4c12fd5a71ec7842492ef5098c444837d47cd565
Cixtor <cixtords@gmail.com>
2019-02-10 09:00:00 -0800

    Add barebone Swift project files to bootstrap application

---
+++ Baburu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Baburu
+//
+//  Created by cixtor on 02/10/19.
+//  Copyright © 2019 cixtor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Baburu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Baburu
+//
+//  Created by cixtor on 02/10/19.
+//  Copyright © 2019 cixtor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
