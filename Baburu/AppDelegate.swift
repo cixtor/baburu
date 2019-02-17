5fd7a137efdf5a14320adf2cf7beb9e3b8193a7b
Cixtor <cixtords@gmail.com>
2019-02-17 09:00:00 -0800

    Modify status bar item logic to comply with modern standards

--- Baburu/AppDelegate.swift
+++ Baburu/AppDelegate.swift
@@ -11,18 +11,30 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var item : NSStatusItem? = nil
+    // Menu Bar Extras
+    //
+    // A menu bar extra exposes app-specific functionality via an icon that
+    // appears in the menu bar when your app is running, even when it’s not the
+    // frontmost app. Menu bar extras are on the opposite side of the menu bar
+    // from your app's menus. The system hides menu bar extras to make room for
+    // app menus. Similarly, if there are too many menu bar extras, the system
+    // may hide some to avoid crowding app menus.
+    //
+    // https://developer.apple.com/design/human-interface-guidelines/macos/extensions/menu-bar-extras/
+    var statusItem: NSStatusItem!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        let statusBar = NSStatusBar.system
 
-        item?.button?.title = "🔔"
+        statusItem = statusBar.statusItem(withLength: NSStatusItem.squareLength)
 
-        let menu = NSMenu()
+        statusItem.button?.title = "🔔"
+
+        let menu = NSMenu(title: "Menu")
         menu.addItem(withTitle: "Preferences", action: #selector(AppDelegate.preferences), keyEquivalent: "")
         menu.addItem(withTitle: "Quit", action: #selector(AppDelegate.quit), keyEquivalent: "")
-        item?.menu = menu
+        statusItem.menu = menu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
