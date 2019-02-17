fe7418615c72cee3670fbcd3066ed5fdb8704da9
Cixtor <cixtords@gmail.com>
2019-02-17 09:00:00 -0800

    Add status item to render a menu bar application

--- Baburu/AppDelegate.swift
+++ Baburu/AppDelegate.swift
@@ -11,16 +11,31 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var item : NSStatusItem? = nil
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        item?.button?.title = "🔔"
+
+        let menu = NSMenu()
+        menu.addItem(withTitle: "Preferences", action: #selector(AppDelegate.preferences), keyEquivalent: "")
+        menu.addItem(withTitle: "Quit", action: #selector(AppDelegate.quit), keyEquivalent: "")
+        item?.menu = menu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    @objc func preferences() {
+        print("Preferences")
+    }
+
+    @objc func quit() {
+        NSApplication.shared.terminate(self)
+    }
 
 }
 
